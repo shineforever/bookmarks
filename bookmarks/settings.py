da40b97f70c7291d8480f50171f0ad287fb12f87
shineforever <shine_forever@yeah.net>
2016-06-28 11:58:37 +0800

    db init

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -75,22 +75,10 @@ WSGI_APPLICATION = 'bookmarks.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-##DATABASES = {
-##    'default': {
-##        'ENGINE': 'django.db.backends.sqlite3',
-##        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-##    }
-##}
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'HOST': '192.168.2.42',
-        'PORT': 33066,
-        'NAME': 'bookmarks',
-        'USER': 'root',
-        'PASSWORD': '123456',
-        'CHARSET': 'utf8'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
