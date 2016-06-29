beb1a7f95d8e7341c98860c6cb005f6a2591ab32
shineforever <shine_forever@yeah.net>
2016-06-29 14:54:31 +0800

    user login and logout

--- account/views.py
+++ account/views.py
@@ -11,6 +11,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from .forms import LoginForm
+from django.contrib.auth.decorators import login_required
 
 def user_login(request):
     """
@@ -35,11 +36,15 @@ def user_login(request):
 
     else:
         form = LoginForm
-    # return HttpResponse('abc')
     return render(request,'account/login.html',{'form':form})
 
 
+@login_required
+def dashboard(request):
+    # return HttpResponse("OK")
 
-
+    return render(request,
+                  'account/dashboard.html',
+                  {'section': 'dashboard'})
 
 
--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+from django.core.urlresolvers import reverse_lazy
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -27,17 +28,20 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+LOGIN_REDIRECT_URL = reverse_lazy('dashboard')
+LOGIN_URL = reverse_lazy('login')
+LOGOUT_URL = reverse_lazy('logout')
 
 # Application definition
 
 INSTALLED_APPS = (
+    'account',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'account',
 )
 
 MIDDLEWARE_CLASSES = (
