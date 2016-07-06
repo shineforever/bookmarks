7eb71013bf66deaf27147a31efc9576f5d52f443
shineforever <shine_forever@yeah.net>
2016-07-06 16:49:05 +0800

    user registration init

--- account/forms.py
+++ account/forms.py
@@ -6,7 +6,35 @@ Created on: 2016-06-28 14:06
 """
 
 from django import forms
+from django.contrib.auth.models import User
 
 class LoginForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
+
+class UserRegistrationForm(forms.ModelForm):
+    """
+    用户注册模块
+    """
+    password = forms.CharField(label='Input your password',widget=forms.PasswordInput)
+    password2 = forms.CharField(label='repeat password',widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ('username','first_name','email')
+
+    def clean_password2(self):
+        """
+        判断两次输入的密码是否一致
+        :return:
+        """
+        cd = self.cleaned_data
+        if cd['password'] != cd['password2']:
+            raise forms.ValidationError("Password don't match")
+        return cd['password2']
+
+
+
+
+
+
