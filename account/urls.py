514847071ec6922399df94ad38dd6fd86950c7d9
root <root@localhost.localdomain>
2016-07-13 09:14:23 +0100

    user register

--- account/urls.py
+++ account/urls.py
@@ -10,13 +10,19 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.dashboard, name='dashboard'),
-    # url(r'^login/$', views.user_login, name='login'),
 
+    #user profile
+    url(r'^register/$',views.register, name='register'),
+
+    #login and logout
     url(r'^login/$','django.contrib.auth.views.login', name='login'),
     url(r'^logout/$','django.contrib.auth.views.logout', name='logout'),
     url(r'^logout-then-login/$','django.contrib.auth.views.logout_then_login', name='logout_then_login'),
+
+    #change password
     url(r'^password-change/$','django.contrib.auth.views.password_change', name='password_change'),
     url(r'^password-change/done/$','django.contrib.auth.views.password_change_done', name='password_change_done'),
+
     # restore password urls
     url(r'^password-reset/$','django.contrib.auth.views.password_reset',name='password_reset'),
     url(r'^password-reset/done/$','django.contrib.auth.views.password_reset_done',name='password_reset_done'),
@@ -24,6 +30,7 @@ urlpatterns = [
        name='password_reset_confirm'),
     url(r'^password-reset/complete/$','django.contrib.auth.views.password_reset_complete',
        name='password_reset_complete'),
+
 
 ]
 
--- account/views.py
+++ account/views.py
@@ -39,6 +39,25 @@ def user_login(request):
         form = LoginForm
     return render(request,'account/login.html',{'form':form})
 
+def register(request):
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+
+        if user_form.is_valid():
+            # Create a new user object but avoid saving it yet
+            new_user = user_form.save(commit=False)
+            # Set the chosen password
+            new_user.set_password(user_form.cleaned_data['password'])
+            # Save the User object
+            new_user.save()
+            # # Create the user profile
+            # profile = Profile.objects.create(user=new_user)
+            return render(request,
+                          'account/register_done.html',
+                          {'new_user': new_user})
+    else:
+        user_form = UserRegistrationForm()
+    return render(request, 'account/register.html', {'user_form': user_form})
 
 @login_required
 def dashboard(request):
